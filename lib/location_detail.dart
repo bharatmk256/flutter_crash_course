4d8701089e8e8267dbf03fc884bebea73a1a3fba
bhartmk257 <bharatmk257@gmail.com>
2018-08-31 01:10:35 +0530

    added images with url

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -15,16 +15,22 @@ class LocationDetail extends StatelessWidget {
       body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _renderFacts(location),
+          children: _renderBody(context, location),
           ),
     );
+    }
+   List<Widget> _renderBody(BuildContext context, Location location){
+       var result  = List<Widget>();
+       result.add(_bannerImage(location.url, 170.0));
+       result.addAll(_renderFacts(context, location));
+    return result;
   }
-  
-  List<Widget> _renderFacts (Location location){
+
+  List<Widget> _renderFacts (BuildContext context, Location location){
     var result  = List<Widget>();
     for (int i = 0; i< location.facts.length;i++){
       result.add(_sectionTitle(location.facts[i].title));
-      result.add(_sectionText(location.facts[i].text));      
+      result.add(_sectionText(location.facts[i].text));  
     }
     return result;
   }
@@ -33,5 +39,11 @@ class LocationDetail extends StatelessWidget {
   }
   Widget _sectionText (String text){
     return Text (text);
+  }
+  Widget _bannerImage(String url, double height, ){
+    return Container(
+
+      child: Image.network(url, fit: BoxFit.fitWidth,)
+    );
   }
 }
--- lib/mocks/mock_location.dart
+++ lib/mocks/mock_location.dart
@@ -5,7 +5,7 @@ class MockLocation extends Location{
   static Location FetchAny(){
     return Location(
       name: 'Taj Mahal India',
-      url: 'https://en.wikipedia.org/wiki/Taj_Mahal',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Taj_Mahal_%28Edited%29.jpeg',
       facts: <LocationFact>[
       LocationFact(
         title:'Summary',
